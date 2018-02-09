8965eda7afcc30e5ab29b31a6378404f4db51852
Kirill Romakhov <kromakhov@yandex.ru>
2018-02-10 01:49:34 +0200

    выпадающее главное меню (неотстилизованное)

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@ $(document).ready(function(){
       nav:true
     });
   });
+
+  var hamburger = document.querySelector("#hamburger__link");
+  hamburger.addEventListener("click", function(event){
+    event.preventDefault();
+    var nav = document.querySelector("#nav-list");
+    nav.classList.add("nav-list_active");
+  });
+
